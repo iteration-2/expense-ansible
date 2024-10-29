a26a751cc6af76f16fa754168db8225b0211697f
pappik <paparao.prema75@gmail.com>
2024-10-29 18:49:41 -0400

    added role wise to pipeline automation

--- ansible.sh
+++ ansible.sh
@@ -1,2 +1,2 @@
 component = $1
-ansible-playbbok -i {componet}-dev.pappik.online, -e ansible_user=ec2-user -e ansible_password=DevOps321
+ansible-playbbok -i $componet-dev.pappik.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=dev -e role_name=$component expense.yml
