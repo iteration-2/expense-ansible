62ac2003e0051d92726bc5eefa1149141d3dcb83
pappik <paparao.prema75@gmail.com>
2024-10-29 19:12:09 -0400

    creating backend and mysql pipelines

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,3 @@
 component = $1
-env = $2
-ansible-playbbok -i $componet-dev.pappik.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=dev -e role_name=$component expense.yml
+#env = $2
+ansible-playbbok -i $componet-$env.pappik.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=$env -e role_name=$component expense.yml
