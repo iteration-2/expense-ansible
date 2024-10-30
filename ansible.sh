106783bbd1752d96e256e202378c203164f7bf41
pappik <paparao.prema75@gmail.com>
2024-10-29 20:13:23 -0400

    component spell correction in asible

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,3 @@
-component = $1
+component=$1
 #env = $2
 ansible-playbook -i $component-$env.pappik.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=$env -e role_name=$component expense.yml
